efc6b96ae6a4fb7edbb8471e2a36328eff0f2988
LaKitKet <LaKitKet@NGITNGETLA>
2024-12-16 16:38:19 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigiGall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
